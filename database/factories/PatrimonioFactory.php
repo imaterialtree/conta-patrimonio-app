718b496b21161e410a0c112d6e4e0c4e7df34bfe
Naoki Miura <naokirafael.miura@gmail.com>
2024-12-05 00:07:39 -0300

    feat(factory): Criar factory de Patrimonio

---
+++ database/factories/PatrimonioFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Classificacao;
+use App\Models\Departamento;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patrimonio>
+ */
+class PatrimonioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'codigo' => fake()->randomNumber(10),
+            'descricao' => fake()->sentence(3),
+            'criado_em' => now(),
+            'atualizado_em' => now(),
+            'departamento_id' => Departamento::factory(),
+            'classificacao_id' => Classificacao::factory(),
+        ];
+    }
+}
