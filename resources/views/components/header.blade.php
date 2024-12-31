0ea8f48fa53bb18cb344e931cf462ac5cf1862f3
Naoki Miura <naokirafael.miura@gmail.com>
2024-12-31 17:32:33 -0300

    refactor: :lipstick: Fixar header e sidebar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,13 @@
 <!-- resources/views/components/header.blade.php -->
-<div class="bg-white p-3 shadow-sm d-flex justify-content-between align-items-center">
+<div class="bg-white p-3 shadow-sm d-flex justify-content-between align-items-center sticky-top">
     <h1 class="h5 mb-0">Inventariado</h1>
     <div class="d-flex align-items-center">
         <i class="bi bi-person-circle fs-4 me-2"></i>
         @auth
             {{-- Dropdown profile --}}
             <div class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->nome }}
                 </a>
 
