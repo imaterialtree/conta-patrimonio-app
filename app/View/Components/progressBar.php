be75053244b2d8a4560ae22e116effd58a22ebf3
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-04 13:07:02 -0300

    renomear componente progressBar para padrão do Laravel

--- app/View/Components/progressBar.php
+++ app/View/Components/progressBar.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class progress_bar extends Component
+class progressBar extends Component
 {
     /**
      * Create a new component instance.
