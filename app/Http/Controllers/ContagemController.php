5b9a4475003d7676a1f168530dfb95871423dfe9
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-05 12:46:42 -0300

    style(controller): Remover phpDocs inúteis dos controllers
    São phpDocs padrões do Laravel Scaffolding. Mas como são métodos padrões da arquitetura e autoexplicativos, esses phpDocs são desnecessários

--- app/Http/Controllers/ContagemController.php
+++ app/Http/Controllers/ContagemController.php
@@ -10,27 +10,18 @@ use Illuminate\Http\Request;
 
 class ContagemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $contagens = Contagem::all();
         return view('contagem.index', compact('contagens'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         $membros = Usuario::all();
         return view('contagem.create', compact('membros'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -46,9 +37,6 @@ class ContagemController extends Controller
         return to_route('contagens.show', $contagem);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Contagem $contagem)
     {
         $departamentos = Departamento::all();
@@ -58,25 +46,16 @@ class ContagemController extends Controller
         return view('contagem.show', compact('contagem', 'departamentos', 'contagemPatrimonios', 'patrimonioTotal'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -11,9 +11,6 @@ use Illuminate\Validation\Rules;
 
 class UsuarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $usuarios = Usuario::all();
@@ -21,53 +18,35 @@ class UsuarioController extends Controller
         return view('usuario.index', compact('usuarios', 'userCount'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('usuario.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(UsuarioRequest $request)
     {
         $user = Usuario::make($request->validated());
         $user->senha = Hash::make('senha123');
         $user->save();
-        
+
         return to_route('usuarios.index')->with('success', 'Usuário criado com sucesso');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Usuario $usuario)
     {
         return view('usuario.show', compact('usuario'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Usuario $usuario)
     {
         return view('usuario.edit', compact('usuario'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Usuario $usuario)
     {
         $usuario->update($request->validated());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Usuario $usuario)
     {
         $usuario->delete();
