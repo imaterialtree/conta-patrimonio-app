8ec8c10a14a5384feb672022766034618db4a2c9
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-09 07:32:44 -0300

    feat(mobile): Completar lógica da home de contagem da comissao
    Permitir ver se há contagem em andamento e se o usuário faz parte dela

--- resources/views/comissao/home.blade.php
+++ resources/views/comissao/home.blade.php
@@ -3,20 +3,29 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-12 col-md-6 text-center">
-            <h3>Contagens em Andamento</h3>
+            <h3>Contagens</h3>
         </div>
     </div>
 
     <!-- Cartão de Contagem em Andamento -->
-    <div class="col-md-4 mb-3">
-        <div class="card border-0 shadow-sm text-center p-4 h-100" style="border-radius: 8px;">
-            <i class="bi bi-clipboard-check" style="font-size: 2rem;"></i>
-            <h5 class="card-title mt-2">Há uma contagem em andamento</h5>
-            <a href="{{ route('contagens.create') }}" class="stretched-link"></a>
+    <div class="col-md-4 mb-3 mx-auto">
+        <div class="card border-0 shadow text-center p-4" style="border-radius: 8px;">
+            @if ($contagem)
+                <i class="bi bi-clipboard-check" style="font-size: 2rem;"></i>
+                <h5 class="card-title mt-2">Há uma contagem em andamento</h5>
+                <a href="{{ route('comissao.contagem.departamentos', $contagem) }}" class="stretched-link"></a>
 
-            <div class="card-body">
-                <p class="text-secondary">Clique aqui para começar a contagem</p>
-            </div>
+                <div class="card-body">
+                    <p class="text-secondary">Clique aqui para começar a contagem</p>
+                @elseif ($contagemExiste)
+                    <i class="bi bi-clipboard-plus" style="font-size: 2rem;"></i>
+                    <h5 class="card-title mt-2">Há uma contagem em andamento, mas você não faz parte da comissão de
+                        contagem
+                    </h5>
+                @else
+                    <i class="bi bi-clipboard-plus" style="font-size: 2rem;"></i>
+                    <h5 class="card-title mt-2">Não há contagem em andamento</h5>
+            @endif
         </div>
     </div>
 @endsection
