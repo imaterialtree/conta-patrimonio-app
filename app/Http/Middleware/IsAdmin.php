1f54a45005678980dca5e32a389938e4a9e16161
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-10 17:08:09 -0300

    fix(middleware): Corrigir bug de IsAdmin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->is_admin) {
+        if (Auth::check() && Auth::user()->isAdmin()) {
             return $next($request);
         }
 
