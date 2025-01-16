309c8e3a6c848814d32e606d09a05ffe04fa92bc
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-16 13:47:57 -0300

    refactor(login): :lipstick: Estilizar tela de login usando componente input e mensagem de erro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,16 +9,19 @@
 </head>
 
 <body>
-    <div class="container vh-100 d-flex justify-content-center align-items-center">
-        @if ($errors->any())
+    @if ($errors->any())
+        <div class="d-flex justify-content-center">
             <x-toast type="danger">
-                <ul>
+                <ul class="list-unstyled">
+                    <i class="bi bi-exclamation-circle text-danger"></i>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
                 </ul>
             </x-toast>
-        @endif
+        </div>
+    @endif
+    <div class="container vh-100 d-flex justify-content-center align-items-center">
 
         <div class="col-lg-4 col-10">
             <div class="text-center">
@@ -32,16 +35,12 @@
             </div>
             <form action="{{ route('login') }}" method="post">
                 @csrf
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email"
-                        required>
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="password" name="senha" placeholder="Senha"
-                        required>
-                </div>
+                <x-input id="email" name="email" type="email" placeholder="Email" with-old-value required>
+                    Email
+                </x-input>
+                <x-input id="password" name="senha" type="password" placeholder="Senha" required>
+                    Senha
+                </x-input>
                 <div class="d-flex justify-content-center mb-3">
                     <a href="#" class="text-decoration-none">Esqueceu a senha?</a>
                 </div>
@@ -49,11 +48,6 @@
                     <button type="submit" class="btn btn-primary w-100">Log in</button>
                 </div>
             </form>
-            <div class="text-center mt-5">
-                <i class="bi bi-boxes fs-1"></i>
-                <p class="mb-0">Conta Patrimônio</p>
-                <small class="text-muted">&copy;2024 All Rights Reserved. Conta Patrimônio<br>Privacy and Terms</small>
-            </div>
         </div>
     </div>
 </body>
