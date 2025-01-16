fbd1ff727b80eb4bf3e79ea983c0c1a2eb16dd75
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-16 18:10:43 -0300

    refactor(mobile): :lipstick: Estilizar view de perfil

--- resources/views/comissao/perfil.blade.php
+++ resources/views/comissao/perfil.blade.php
@@ -3,26 +3,29 @@
 @section('content')
     <div class="card mx-auto">
         <div class="card-body">
-            <h2 class="card-title h4 mb-4">Visualizar Usuário</h2>
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome de Usuário</label>
-                <p id="nome" class="form-control">{{ $usuario->nome }}</p>
+            <h2 class="card-title h4 mb-4">Perfil</h2>
+            <div class="d-flex flex-column align-items-center mb-4">
+                {{-- <img src="{{ asset(auth()->user()->icone) }}" alt="User Icon" class="rounded-circle" width="100" height="100"> --}}
+                <i class="fas fa-user-circle fa-3x mb-2"></i>
+                <a href="#">Alterar foto de perfil</a>
             </div>
-
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <p id="email" class="form-control">{{ $usuario->email }}</p>
-            </div>
-
-            <div class="mb-3">
-                <label for="siape" class="form-label">SIAPE</label>
-                <p id="siape" class="form-control">{{ $usuario->siape }}</p>
-            </div>
-
-            <div class="mb-3">
-                <label for="tipo" class="form-label">Tipo</label>
-                <<p id="siape" class="form-control">{{ $usuario->tipo }}</p>
-            </div>
+            <ul class="list-group list-group-flush mb-3">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <strong>Nome de Usuário:</strong>
+                    <span>{{ $usuario->nome }}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <strong>Email:</strong>
+                    <span>{{ $usuario->email }}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <strong>SIAPE:</strong>
+                    <span>{{ $usuario->siape }}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <strong>Tipo:</strong>
+                    <span>{{ $usuario->tipo }}</span>
+                </li>
+            </ul>
         </div>
-    </div>
-@endsection
+    @endsection
