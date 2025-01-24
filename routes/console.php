d6ba7e7e0a99c12d59d984771ba741a026e756bb
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-24 18:43:49 -0300

    feat(console): adicionar comando para exibir o arquivo .env atualmente em uso

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('env:file', function () {
+    $environmentFile = app()->environmentFile();
+        $this->info("Arquivo env atual: $environmentFile");
+        return 0;
+})->purpose('Informa qual o arquivo env atual');
