69a0982f165d22bb46699c20a92d297a4917dd3a
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-25 22:33:55 -0300

    refactor(sidebar): adicionar classe 'active' às links de navegação para indicar a página atual

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,32 +2,37 @@
     <h4 class="mb-4">Opções</h4>
     <ul class="nav flex-column">
         <li class="nav-item mb-2">
-            <a href="{{ route('home') }}" class="nav-link text-dark">
+            <a href="{{ route('home') }}" class="nav-link {{ request()->routeIs('home') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-house-door-fill me-2"></i> Página Inicial
             </a>
         </li>
         <li class="nav-item mb-2">
-            <a href="{{ route('contagens.index') }}" class="nav-link text-dark">
+            <a href="{{ route('contagens.index') }}"
+                class="nav-link {{ request()->routeIs('contagens.index') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-list-check me-2"></i> Contagem
             </a>
         </li>
         <li class="nav-item mb-2">
-            <a href="{{ route('relatorios.index') }}" class="nav-link text-dark">
+            <a href="{{ route('relatorios.index') }}"
+                class="nav-link {{ request()->routeIs('relatorios.index') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-file-earmark-text me-2"></i> Gerar Relatório
             </a>
         </li>
         <li class="nav-item mb-2">
-            <a href="{{ route('patrimonios.index') }}" class="nav-link text-dark">
+            <a href="{{ route('patrimonios.index') }}"
+                class="nav-link {{ request()->routeIs('patrimonios.index') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-box-seam me-2"></i> Gerenciar Patrimônio
             </a>
         </li>
         <li class="nav-item mb-2">
-            <a href="{{ route('usuarios.index') }}" class="nav-link text-dark">
+            <a href="{{ route('usuarios.index') }}"
+                class="nav-link {{ request()->routeIs('usuarios.index') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-people-fill me-2"></i> Gerenciar Usuários
             </a>
         </li>
         <li class="nav-item mb-2">
-            <a href="{{ route('departamentos.index') }}" class="nav-link text-dark">
+            <a href="{{ route('departamentos.index') }}"
+                class="nav-link {{ request()->routeIs('departamentos.index') ? 'active' : 'text-dark' }}">
                 <i class="bi bi-building me-2"></i> Departamentos
             </a>
         </li>
