14650d674a1ddca52de8c20e77b3bf286000876d
Naoki Miura <naokirafael.miura@gmail.com>
2025-01-27 09:19:57 -0300

    feat(vite): atualizar entradas de JavaScript para incluir novos arquivos de leitura e contagem RFID

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/rfid.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/js/rfid-contagem.js',
+                'resources/js/rfid-leitura.js'
+            ],
             refresh: true,
         }),
     ],
